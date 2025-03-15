1655ba06d8c95928fca62070c8bb286a25108269
Yunus YAVUZ <yyavuz15@ford.com.tr>
2025-03-15 14:13:42 +0300

    Add new app icons and remove deprecated icon files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.mescidgo.mescid_go"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
